8d78df589ec2234726343b072313e6f2135492a1
EgorovaEE <Liza12061996@rambler.ru>
2018-11-26 10:46:09 +0500

    Update cols.py

--- lab1/cols.py
+++ lab1/cols.py
@@ -1,14 +1,28 @@
 from parserP import stih
 
-count=0
+def cols(stih):
 
-while stih.count("\n\n") != 0:
-    stih = stih.replace("\n\n", "\n")
+    count=0
 
-#print(stih)
-#print(stih.count("\n"))
+    if stih != 'false':
 
-if stih.count("\n") == 14:
-    cols_res = "Скорей всего, это сонет, написанный "
-else:
-    cols_res = "Скорей всего, это стихотворение, написанное "
+        while stih.count("\n\n") != 0:
+            stih = stih.replace("\n\n", "\n")
+
+        smas = stih.split("\n")
+
+        count_long_strok = 0
+        for smasi in smas:
+            if len(smasi) > 70:
+                count_long_strok = count_long_strok + 1
+
+        if (len(smas)//2 < count_long_strok):
+            return 'false'
+        else:
+            if len(smas) == 14:
+                return "Скорей всего, это сонет, написанный "
+            else:
+                if len(smas) > 200:
+                    return 'false'
+                else:
+                    return "Скорей всего, это стихотворение, написанное "
